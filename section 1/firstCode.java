1495daf004dbf9f788f7dd1e9eed72de814901ae
Ajay Mathew <ajayedathazhe@gmail.com>
2022-09-15 17:17:16 +0530

    section 1 completed

---
+++ section 1/firstCode.java
@@ -0,0 +1,12 @@
+
+class firstCode{
+    public static void main(String args[]){
+        System.out.println("First code");
+    }
+}
+
+
+// in order to compile the code follow the procedures below:
+// first move to the file through the cmd
+// secondly use the command 'javac filename.java' to compile the code
+// then use the command 'java filename' to run the code
